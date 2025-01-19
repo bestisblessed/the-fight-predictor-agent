1d86641ba191e8135f17b430b8671b0f68ddc264
bestisblessed <tyler.durette@gmail.com>
2025-01-19 06:49:06 -0500

    im dumb

--- PRODUCTION/run_agent_cron.sh
+++ PRODUCTION/run_agent_cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 touch $HOME/the-fight-predictor-agent/PRODUCTION/cron.log
+echo "------------------------------------------------" >> $HOME/the-fight-predictor-agent/PRODUCTION/cron.log
 echo "Starting Agent: $(date)" >> $HOME/the-fight-predictor-agent/PRODUCTION/cron.log
 
 cd $HOME/the-fight-predictor-agent/PRODUCTION
@@ -8,3 +9,4 @@ $HOME/.pyenv/shims/python download_mentions_from_drive.py
 $HOME/.pyenv/shims/python assistant_from_tweets.py
 
 echo "Completed Agent: $(date)" >> $HOME/the-fight-predictor-agent/PRODUCTION/cron.log
+echo "------------------------------------------------" >> $HOME/the-fight-predictor-agent/PRODUCTION/cron.log
