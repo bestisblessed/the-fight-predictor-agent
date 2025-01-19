621e56f59ca3d31a6013cd5ce8a6a5b17a88fd9b
bestisblessed <tyler.durette@gmail.com>
2025-01-19 06:55:06 -0500

    im dumb

--- run_agent.sh
+++ run_agent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python X/download_mentions_from_drive.py
+python download_mentions_from_drive.py
 # cp data/TheFightAgentMentions.docx data/TheFightAgentMentionsWorking.docx
 python assistant_from_tweets.py
--- run_agent_cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "=== Starting Fight Predictor Agent at $(date) ===" >> /home/trinity/the-fight-predictor-agent/cron.log
-
-cd /home/trinity/the-fight-predictor-agent
-/home/trinity/.pyenv/shims/python X/download_mentions_from_drive.py
-/home/trinity/.pyenv/shims/python assistant_from_tweets.py
-
-echo "=== Completed Fight Predictor Agent at $(date) ===" >> /home/trinity/the-fight-predictor-agent/cron.log
